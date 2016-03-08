f25408d3914bcc90ebc04e3b300435624f4adfc3
pshah1995 <pshah@caltech.edu>
2016-03-08 00:50:49 -0800

    pushing code that can beat simple player

--- player.cpp
+++ player.cpp
@@ -1,4 +1,6 @@
 #include "player.h"
+#include <iostream>
+#include <cmath>
 
 /*
  * Constructor for the player; initialize everything here. The side your AI is
@@ -7,16 +9,19 @@
  */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
+    //std:cerr << "blah" << std::endl;
+	//fprintf(stderr, "%d\n", "123");
     testingMinimax = false;
-    Board board = new board();
-    int turn;
-    if(side == "BLACK")
+    board = new Board();
+    me = side;
+    move = NULL;
+    if (side == BLACK)
     {
-    	turn = 1;
+    	opp = WHITE;
     }
     else
     {
-    	turn = 2;
+    	opp = BLACK;
     }
 
     /* 
@@ -49,5 +54,91 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */ 
-    return NULL;
+    board->doMove(opponentsMove, opp);
+
+    return heuristic();
 }
+
+Move *Player::heuristic() {
+	//int score_array [8][8];
+    int index_x = -1;
+    int index_y = -1;
+    double score = (-3*64)+1;
+    double score_temp = 0;
+    double score_prev = board->count(me) - board->count(opp);
+    
+    //std::cerr << "in move" << std::endl;
+    for(int i = 0; i < 8; i++)
+    {
+    	for(int j = 0; j < 8; j++)
+    	{
+    		//std::cerr << "in loop" << std::endl;
+    		Move *move_temp = new Move(i, j);
+    		if(board->checkMove(move_temp, me))
+    		{
+    			//std::cerr << "in true" << std::endl;
+    		    Board *board_1 = board->copy();
+    		    board_1->doMove(move_temp, me);
+
+    			if(i == 1 || i == 6 || j == 1 || j == 6)
+    			{
+    				score_temp = (0.33) * (board_1->count(me) - board_1->count(opp) - score_prev);
+    				if(score_temp > score)
+    				{
+    					score = score_temp;
+    					index_x = i;
+    					index_y = j;
+    				}
+    			}
+    			else if(i == 0 || i == 7 || j == 0 || j == 7)
+    			{
+    				score_temp = 3 * (board_1->count(me) - board_1->count(opp) - score_prev);
+    				if(score_temp > score)
+    				{
+    					score = score_temp;
+    					index_x = i;
+    					index_y = j;
+    				}
+    			}
+    			else if((i == 0 && j == 0) || (i == 0 && j == 7) || (i == 7 && j == 0) || (i == 7 && j == 7))
+    			{
+    				score_temp = 5 * (board_1->count(me) - board_1->count(opp) - score_prev);
+    				if(score_temp > score)
+    				{
+    					score = score_temp;
+    					index_x = i;
+    					index_y = j;
+    				}    				
+    			}    			
+    			else
+    			{
+    				score_temp = (board_1->count(me) - board_1->count(opp) - score_prev);
+    				if(score_temp > score)
+    				{
+    					score = score_temp;
+    					index_x = i;
+    					index_y = j;
+    				}    				
+    			}
+    		delete(board_1);
+    		//std::cerr << score << std::endl;
+    		}
+    		delete(move_temp);
+    	}
+    }
+ 
+    if(index_x == -1)
+    {
+    	return NULL;
+    }
+    else 
+    {
+        Move *myMove = new Move(index_x, index_y);
+    	board->doMove(myMove, me);    	
+    	return myMove;
+    }
+}
+
+//Move *Player::minimax() {
+
+//}
--- player.h
+++ player.h
@@ -13,9 +13,16 @@ public:
     ~Player();
     
     Move *doMove(Move *opponentsMove, int msLeft);
+    Move *heuristic();
+    //Move *minimax();
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
+
+    Board *board;
+    Side opp, me;
+    Move *move;
+
 };
 
 #endif
